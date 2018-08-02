f74bdbcf154f27f014012f9c4c196e57e696f3c9
Dave O'Keeffe <dave@marinosoftware.com>
2018-08-02 17:08:47 +0100

    Begin work on refactoring out the onload listeners, in favour of event delegation

--- app/javascript/active_storage_drag_and_drop/ujs.js
+++ app/javascript/active_storage_drag_and_drop/ujs.js
@@ -1,5 +1,6 @@
 import { UploadQueueProcessor } from "./upload_queue_processor"
 import { UploaderInput } from "./uploader_input"
+import { uploaders, createUploader } from "./direct_upload_controller"
 
 let started = false
 let formSubmitted = false
@@ -44,18 +45,63 @@ function handleFormSubmissionEvent(event) {
   }
 }
 
-function initializeDragAndDropUploader() {
-  formSubmitted = false
-  document.querySelectorAll('input[type=file][data-dnd=true]').forEach(input => {
-    new UploaderInput(input)
-  })
-}
-
 export function start() {
   if (started) { return }
   started = true
   document.addEventListener("submit", didSubmitForm)
   document.addEventListener("ajax:before", didSubmitRemoteElement)
-  addEventListener("turbolinks:load", initializeDragAndDropUploader)
-  addEventListener("load", initializeDragAndDropUploader)
+
+  // input[type=file][data-dnd=true]
+  document.addEventListener("change", event => {
+    const input = event.target;
+    if(input.type == 'file' && input.dataset.dataDnd == 'true') {
+      console.log('input:change')
+      Array.from(input.files).forEach(file => createUploader(input, file))
+      input.value = null
+    }
+  })
+  document.addEventListener("dragover", event => {
+    const target = event.target;
+    if(target.classList.contains('asdndzone')) {
+      event.preventDefault();
+    }
+  })
+  document.addEventListener("drop", event => {
+    const target = event.target;
+    if(target.classList.contains('asdndzone')) {
+      event.preventDefault()
+      // get the input associated with this dndz
+      const input = document.getElementById(target.dataset.dndInputId)
+      Array.from(event.dataTransfer.files).forEach(file => createUploader(input, file))
+    }
+  })
+  document.addEventListener("click", event => {
+    const target = event.target;
+    if(target.classList.contains('asdndzone')) {
+      if( target.dataset.dndDelete == 'true' && target.hasAttribute('data-direct-upload-id') ) {
+        event.preventDefault();
+        console.log("Delete Uploaded File: " + target.dataset.directUploadId)
+        document.querySelectorAll('[data-direct-upload-id="'+target.dataset.directUploadId+'"]').forEach(element => {
+          element.remove()
+        })
+        for(var i=0;i<uploaders.length;i++) {
+          if(uploaders[i].upload.id == target.dataset.directUploadId) {
+            uploaders.splice( i, 1 )
+            break
+          }
+        }
+      }
+    }
+  })
+  // TODO: onload preparation of the pre-existing uploads
+  // iconContainer isn't accounted for yet
+  // document.querySelectorAll("input[type='hidden'][data-direct-upload-id][data-uploaded-file-name]").forEach( uploadedFile => {
+  //   const dataset = uploadedFile.dataset
+  //   let detail = {
+  //     id: dataset.directUploadId,
+  //     fileName: dataset.uploadedFileName,
+  //     iconContainer: iconContainer
+  //   }
+  //   dispatchEvent(uploadedFile, `${eventFamily}:placeholder`, { detail })
+  // })
 }
