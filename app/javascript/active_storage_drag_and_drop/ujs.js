761beb452de5384e5e8b1205920e986f0470d88b
Dave O'Keeffe <dave@marinosoftware.com>
2018-08-02 13:54:12 +0100

    Add double-submit protection in init dnd uploader

--- app/javascript/active_storage_drag_and_drop/ujs.js
+++ app/javascript/active_storage_drag_and_drop/ujs.js
@@ -2,6 +2,7 @@ import { UploadQueueProcessor } from "./upload_queue_processor"
 import { UploaderInput } from "./uploader_input"
 
 let started = false
+let formSubmitted = false
 
 function didSubmitForm(event) {
   handleFormSubmissionEvent(event)
@@ -14,6 +15,8 @@ function didSubmitRemoteElement(event) {
 }
 
 function handleFormSubmissionEvent(event) {
+  if(formSubmitted) { return }
+  formSubmitted = true
   const form = event.target
   const next_upload = new UploadQueueProcessor(form)
   // if the upload processor has no dnd file inputs, then we let the event happen naturally
@@ -25,29 +28,34 @@ function handleFormSubmissionEvent(event) {
       if (error) {
         // inputs.forEach(enable)
       } else {
-        // The original ActiveStorage DirectUpload system did this action using
-        // input.click(), but doing that either makes the form submission event
-        // happen multiple times, or the browser seems to block the input.click()
-        // event completely, because it's not a trusted 'as a result of a mouse
-        // click' event.
-        form.submit()
+         form.submit()
+         // The original ActiveStorage DirectUpload system did this action using
+         // input.click(), but doing that either makes the form submission event
+         // happen multiple times, or the browser seems to block the input.click()
+         // event completely, because it's not a trusted 'as a result of a mouse
+         // click' event.
+         // HOWEVER
+         // form.submit() doesn't trigger to any UJS submission events. This
+         // results in remote forms being submitted locally whenever there's a
+         // dnd file to upload.  Instead we use Rails.fire(element, 'submit')
+         // Rails.fire(form, 'submit')
       }
     })
   }
 }
 
 function initializeDragAndDropUploader() {
+  formSubmitted = false
   document.querySelectorAll('input[type=file][data-dnd=true]').forEach(input => {
     new UploaderInput(input)
   })
 }
 
 export function start() {
-  if (!started) {
-    started = true
-    document.addEventListener("submit", didSubmitForm)
-    document.addEventListener("ajax:before", didSubmitRemoteElement)
-    addEventListener("turbolinks:load", initializeDragAndDropUploader)
-    addEventListener("load", initializeDragAndDropUploader)
-  }
+  if (started) { return }
+  started = true
+  document.addEventListener("submit", didSubmitForm)
+  document.addEventListener("ajax:before", didSubmitRemoteElement)
+  addEventListener("turbolinks:load", initializeDragAndDropUploader)
+  addEventListener("load", initializeDragAndDropUploader)
 }
