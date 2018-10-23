9de537d17fdd1c8a7a246ebb7cb89747f0c91335
Ian Grant <ian.grant@marinosoftware.com>
2018-10-23 14:42:00 +0100

    Allow users to pass options for the nested file field into the drag and drop field

--- lib/active_storage_drag_and_drop/rails/form_builder.rb
+++ lib/active_storage_drag_and_drop/rails/form_builder.rb
@@ -11,8 +11,30 @@ module ActiveStorageDragAndDrop
                            tag_value: tag_value,
                            form: self,
                            attachments: @object.send(method),
-                           method: method
+                           method: method,
+                           options: file_field_options(options, method)
                          }
+      end
+
+      private
+
+      def default_file_field_options(method)
+        {
+          multiple: true,
+          direct_upload: true,
+          style: 'display:none;',
+          data: {
+            dnd: true,
+            dnd_zone_id: "asdndz-#{object_name}_#{method}",
+            icon_container_id: "asdndz-#{object_name}_#{method}__icon-container"
+          }
+        }
+      end
+
+      def file_field_options(custom_options, method)
+        default_file_field_options(method).merge(custom_options) do |_key, default, custom|
+          default.is_a?(Hash) && custom.is_a?(Hash) ? default.merge(custom) : custom
+        end
       end
     end
   end
