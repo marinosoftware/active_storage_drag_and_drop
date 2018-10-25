1eebb293e4bbed3be382682e078d29bc2905ecdb
Ian Grant <ian.grant@marinosoftware.com>
2018-10-25 13:58:26 +0100

    Add method to display human readable byte sizes

--- app/javascript/active_storage_drag_and_drop/upload_queue_processor.js
+++ app/javascript/active_storage_drag_and_drop/upload_queue_processor.js
@@ -64,3 +64,17 @@ export function createUploader(input, file) {
     return event
   }
 }
+
+function humanFileSize (bytes) {
+  var thresh = 1000
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B'
+  }
+  var units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  var u = -1
+  do {
+    bytes /= thresh
+    ++u
+  } while (Math.abs(bytes) >= thresh && u < units.length - 1)
+  return bytes.toFixed(1) + ' ' + units[u]
+}
