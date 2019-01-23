170a22d1accebf5d5e1e485abe580a9fa4378417
Ian Grant <ian.grant@marinosoftware.com>
2019-01-23 14:36:06 +0000

    Add YARD documentation for rubygems

--- lib/active_storage_drag_and_drop.rb
+++ lib/active_storage_drag_and_drop.rb
@@ -1,7 +1,15 @@
 require 'active_storage_drag_and_drop/version'
 require 'active_storage_drag_and_drop/form_builder'
 
+# Gem namespace
+# @since 0.1.0
 module ActiveStorageDragAndDrop
+  # Inherits from Rails::Engine to allow us to mount the packaged javascript/css files and add a
+  # custom initializer to execute custom FormBuilder code in the context of
+  # ActionView::Helpers::FormBuilder
+  #
+  # @author Ian Grant
+  # @since 0.1.0
   class Engine < Rails::Engine
     initializer 'active_storage_drag_and_drop.form_builder' do |_app|
       ActiveSupport.on_load(:action_view) do
--- lib/active_storage_drag_and_drop/form_builder.rb
+++ lib/active_storage_drag_and_drop/form_builder.rb
@@ -1,7 +1,44 @@
 module ActiveStorageDragAndDrop
+  # Custom FormBuilder module. All code in this module is executed within the context of
+  # ActionView::Helpers::FormBuilder when ActionView is first loaded via the
+  # {Engine Engine}
+  # @since 0.1.0
   module FormBuilder
     delegate :capture, :content_tag, :tag, :safe_join, to: :@template
 
+    # Returns a file upload input tag wrapped in markup that allows dragging and dropping of files
+    # onto the element.
+    #
+    # @author Ian Grant
+    # @see file:README.md#Usage Usage section of the README
+    #
+    # @param [Symbol] method The attribute on the target model to attach the files to.
+    # @param [String] content The content to render inside of the drag and drop file field.
+    # @param [Hash] options A hash of options to customise the file field.
+    #
+    # @option options [Boolean] :disabled If set to true, the user will not be able to use this
+    #   input.
+    # @option options [Boolean] :mutiple If set to true, *in most updated browsers* the user will
+    #   be allowed to select multiple files.
+    # @option options [String] :accept If set to one or multiple mime-types, the user will be
+    #   suggested a filter when choosing a file. You still need to set up model validations.
+    # @option options [Integer] :size_limit The upper limit on filesize to accept in bytes.
+    #   Client-side validation only. You still need to set up model validations.
+    #
+    # @return [String] The generated file field markup.
+    #
+    # @example
+    #   # Accept only PNGs or JPEGs up to 5MB in size:
+    #   form.drag_and_drop_file_field :images, nil, accept: 'image/png, image/jpeg',
+    #                                               size_limit: 5_000_000
+    # @example
+    #   # Pass custom content string:
+    #    form.drag_and_drop_file_field :images, '<div>Drag and Drop!</div>', accept: 'image/png'
+    # @example
+    #   # Pass a block of content instead of passing a string
+    #   <%= form.drag_and_drop_file_field(:images, accept: 'image/png') do %>
+    #     <strong>Drag and Drop</strong> PNG files here or <strong>click to browse</strong>
+    #   <% end %>
     def drag_and_drop_file_field(method, content_or_options = nil, options = {}, &block)
       if block_given?
         options = content_or_options if content_or_options.is_a? Hash
@@ -13,6 +50,15 @@ module ActiveStorageDragAndDrop
 
     private
 
+    # After {#drag_and_drop_file_field} has parsed whether the content was passed as a block or a
+    # parameter the result is passed to this method which actually generates the markup.
+    #
+    # @author Ian Grant
+    # @see #drag_and_drop_file_field
+    #
+    # @param (see #drag_and_drop_file_field)
+    # @option (see #drag_and_drop_file_field)
+    # @return (see #drag_and_drop_file_field)
     def drag_and_drop_file_field_string(method, content = nil, options = {})
       ref = "#{object_name}_#{method}"
       content = [content]
@@ -23,6 +69,13 @@ module ActiveStorageDragAndDrop
                                               'data-dnd-input-id': ref
     end
 
+    # Checks for unpersisted file attachments (e.g. left over after a failed validation) and
+    # returns an array of tags used to pre-populate the the dropzone with tags queueing those files
+    # for attachment at the next form submission.
+    #
+    # @author Ian Grant
+    # @param [Symbol] method The attribute on the target model to attach the files to.
+    # @return [Array] An array of hidden field tags for each unpersisted file attachment.
     def unpersisted_attachment_fields(method)
       attachments = @object.send(method).reject(&:persisted?)
       attachments.map.with_index do |blob, idx|
@@ -36,6 +89,11 @@ module ActiveStorageDragAndDrop
       end
     end
 
+    # Generates a hash of default options for the embedded file input field.
+    #
+    # @author Ian Grant
+    # @param [Symbol] method The attribute on the target model to attach the files to.
+    # @return [Hash] The default options  for the file field
     def default_file_field_options(method)
       {
         multiple: true,
@@ -49,6 +107,13 @@ module ActiveStorageDragAndDrop
       }
     end
 
+    # Merges the user provided options with the default options overwriting the defaults to
+    # generate the final options passed to the embedded file input field.
+    #
+    # @author Ian Grant
+    # @param [Symbol] method The attribute on the target model to attach the files to.
+    # @param [Hash] custom_options The user provided custom options hash.
+    # @return [Hash] The user provided options and default options merged.
     def file_field_options(method, custom_options)
       default_file_field_options(method).merge(custom_options) do |_key, default, custom|
         default.is_a?(Hash) && custom.is_a?(Hash) ? default.merge(custom) : custom
