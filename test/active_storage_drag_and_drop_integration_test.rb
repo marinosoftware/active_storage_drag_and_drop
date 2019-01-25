d9e557fb655cae8580c7ad84f1ded8b43f19e265
Ian Grant <ian.grant@marinosoftware.com>
2019-01-25 15:19:56 +0000

    Add integration tests for requeing of attachment of already uploaded files that didn't pass validation

--- test/active_storage_drag_and_drop_integration_test.rb
+++ test/active_storage_drag_and_drop_integration_test.rb
@@ -11,10 +11,21 @@ end
 # app/models/user.rb
 class User < ApplicationRecord
   has_many_attached :highlights
+  validate :highlights_validation
+
+  def highlights_validation
+    highlights.each do |highlight|
+      next unless highlight.blob.filename == 'invalid.txt'
+
+      highlight.purge
+      errors[:base] << 'This file is invalid'
+    end
+  end
 end
 
 # app/views/users/new.html.erb
 USERS_NEW_TEMPLATE = <<-HTML
+  <h1><%= flash['error'] %></h1>
   <%= form_with model: @user, url: user_path do |form| %>
     <%= form.drag_and_drop_file_field(:highlights) do %>
       <strong>Drag and Drop</strong> files here or <strong>click to browse</strong>
@@ -30,6 +41,16 @@ USERS_NEW_TEMPLATE = <<-HTML
   </script>
 HTML
 
+# app/views/users/show.html.erb
+USERS_SHOW_TEMPLATE = <<-HTML
+  <h1>Highlights</h1>
+  <ul>
+    <% @user.highlights.each do |highlight| %>
+      <li><%= highlight.filename %></li>
+    <% end %>
+  </ul>
+HTML
+
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
   def new
@@ -38,8 +59,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(user_params)
-    render inline: USERS_NEW_TEMPLATE, layout: 'application'
+    @user = User.new(user_params)
+    if @user.save
+      render inline: USERS_SHOW_TEMPLATE, layout: 'application'
+    else
+      flash.now['error'] = @user.errors.full_messages
+      render inline: USERS_NEW_TEMPLATE, layout: 'application'
+    end
   end
 
   private
@@ -66,5 +92,21 @@ class ActiveStorageDragAndDropIntegrationTest < ActionDispatch::IntegrationTest
     attach_file 'user_highlights', file_path('arrow.png'), make_visible: true
     click_on 'Create User'
     assert_match 'arrow.png', User.last.highlights.first.filename.to_s
+  end
+
+  test 'hidden file field can attach multiple files' do
+    visit 'user/new'
+    attach_file 'user_highlights', file_path('arrow.png'), make_visible: true
+    attach_file 'user_highlights', file_path('document.txt'), make_visible: true
+    click_on 'Create User'
+    assert_equal 2, User.last.highlights.count
+  end
+
+  test 'files which fail to be attached for any reason are requeued for attachment' do
+    visit 'user/new'
+    attach_file 'user_highlights', file_path('arrow.png'), make_visible: true
+    attach_file 'user_highlights', file_path('invalid.txt'), make_visible: true
+    click_on 'Create User'
+    page.assert_selector '.direct-upload--complete', count: 2
   end
 end
